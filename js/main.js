b0754e3bbed0897f03bc0eac543b8e04fd57fe6b
Ajith01617 <ajithasha0085@gmail.com>
2026-02-06 14:25:13 +0530

    ..

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function toggleControls() {
+  document
+    .querySelector('.vlab-controls')
+    .classList.toggle('active');
+}
+
+function toggleLogs() {
+  document
+    .querySelector('.vlab-logs')
+    .classList.toggle('active');
+}
